80a1646e9cb9599950f6d8aa771dfb0490bc53f1
anton-pozhidaev <anton.pozhidaev@gmail.com>
2020-11-26 08:19:51 +0300

    release candidate of completed task 1 are presented

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,12 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.sql.Time;
+
+@Component("timerBean")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
